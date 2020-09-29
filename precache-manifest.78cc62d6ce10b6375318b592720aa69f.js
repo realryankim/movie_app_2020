52fa931c43d8619e937c64b16b91336a5fb80ac4
realryankim <ryankimofficial@gmail.com>
2020-09-29 18:25:20 +0900

    Updates

---
+++ precache-manifest.78cc62d6ce10b6375318b592720aa69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058646d84b659ee6cb6f08372873b70c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "cbb28a23df2065b870e8",
+    "url": "/movie_app_2020/static/css/main.3b34c982.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb28a23df2065b870e8",
+    "url": "/movie_app_2020/static/js/main.729b6003.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
